f0dbcf91c189f54432b115830e68224a389bb9f9
AzureAD\JesusArockiaSankaran <jesusarockias@syncfusion.com>
2021-03-16 19:11:38 +0530

    initial commit

--- src/app.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports", "@syncfusion/ej2-buttons"], function (require, exports, ej2_buttons_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var button = new ej2_buttons_1.Button({ content: 'Button' });
-    button.appendTo('#element');
-});
-//# sourceMappingURL=app.js.map
